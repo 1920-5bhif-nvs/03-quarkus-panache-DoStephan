8cff6c92a0166764ac105c99d9c7d1fd8b17ed13
DoStephan <stephan.do08@gmail.com>
2019-11-19 09:15:10 +0100

    initialize project

---
+++ quarkus-panache/src/test/java/at/htl/NativeTestResourceIT.java
@@ -0,0 +1,9 @@
+package at.htl;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeTestResourceIT extends TestResourceTest {
+
+    // Execute the same tests but in native mode.
+}
