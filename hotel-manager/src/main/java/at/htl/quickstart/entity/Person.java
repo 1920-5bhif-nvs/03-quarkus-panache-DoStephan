8e1ebe592521479bb424f82757e1c4cebbf9df6b
DoStephan <stephan.do08@gmail.com>
2019-12-08 18:03:09 +0100

    Completed entity Person

--- hotel-manager/src/main/java/at/htl/quickstart/entity/Person.java
+++ hotel-manager/src/main/java/at/htl/quickstart/entity/Person.java
@@ -1,5 +1,34 @@
 package at.htl.quickstart.entity;
 
-//@Entity
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Person {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
 }
