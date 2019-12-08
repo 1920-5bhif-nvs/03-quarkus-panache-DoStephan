c6d2adf391550a0bd5fbef30709dbe3b48018243
DoStephan <stephan.do08@gmail.com>
2019-12-08 18:58:32 +0100

    Updated InitBean

--- hotel-manager/src/main/java/at/htl/quickstart/control/InitBean.java
+++ hotel-manager/src/main/java/at/htl/quickstart/control/InitBean.java
@@ -1,6 +1,8 @@
 package at.htl.quickstart.control;
 
 import at.htl.quickstart.entity.Guest;
+import at.htl.quickstart.entity.Hotel;
+import at.htl.quickstart.entity.Room;
 import io.quarkus.runtime.StartupEvent;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -8,6 +10,8 @@ import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @ApplicationScoped
 public class InitBean {
@@ -17,6 +21,18 @@ public class InitBean {
 
     @Transactional
     public void init(@Observes StartupEvent ev){
+        initHotel();
+        initGuest();
+        initRoom();
+    }
+
+    private void initRoom() {
+        for (int i = 1; i < 50; i++) {
+            em.persist(new Room(i));
+        }
+    }
+
+    private void initGuest() {
         Guest[] guests = {
                 new Guest("Steve"),
                 new Guest("Nero"),
@@ -24,6 +40,16 @@ public class InitBean {
         };
         for (Guest guest : guests) {
             em.persist(guest);
+        }
+    }
+
+    private void initHotel() {
+        Hotel[] hotels = {
+                new Hotel("Ibis"),
+                new Hotel("Meininger Hotel"),
+        };
+        for (Hotel hotel : hotels) {
+            em.persist(hotel);
         }
     }
 }
